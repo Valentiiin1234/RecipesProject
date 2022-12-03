bfbdca118a9d7974d17934b76da0af66125696be
Valentin <vaminov94@gmail.com>
2022-12-04 00:47:04 +0300

    storyboard

---
+++ RecipesProject/AuthorizationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AuthorizationViewController.swift
+//  RecipesProject
+//
+//  Created by Mac on 02.12.2022.
+//
+
+import UIKit
+
+class AuthorizationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
